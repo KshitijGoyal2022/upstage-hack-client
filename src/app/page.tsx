d669c403076a471e2950d068256c175028f4a754
Kshitij Goyal <117494616+KshitijGoyal2022@users.noreply.github.com>
2024-08-12 22:45:20 -0700

    small fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className='flex justify-center items-center min-h-screen'>
-      <div className='grid grid-cols-2 gap-4'>
+    <div className='flex justify-center items-center min-h-screen p-4'>
+      <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-4'>
         <div
-          className='w-[400px] flex flex-col p-10 rounded-lg'
+          className='w-full md:w-[400px] flex flex-col p-6 md:p-10 rounded-lg'
           style={{ backgroundColor: '#F8F9FB' }}
         >
           <div className='flex flex-col'>
@@ -19,7 +19,7 @@ export default function Home() {
               <ChevronRight />
             </Link>
           </div>
-          <div className='flex justify-center'>
+          <div className='flex justify-center mt-4'>
             <Image
               src="/chat.svg" 
               alt="AI Chat Icon"
@@ -29,20 +29,25 @@ export default function Home() {
           </div>
         </div>
 
+        {/* OR Text for Small Screens */}
+        <div className="flex justify-center items-center sm:block md:hidden">
+          <p className="text-lg ">OR</p>
+        </div>
+
         <div className='flex flex-col space-y-4'>
           <div
-            className='flex justify-between items-center p-10 rounded-lg'
+            className='flex justify-between items-center p-6 md:p-10 rounded-lg'
             style={{ backgroundColor: '#F8F9FB' }}
           >
             <div>
               <p className='text-lg font-medium'>I just want to browse</p>
-              <Link className='text-gray-600 mt-2 flex' href='/'>
+              <Link className='text-gray-600 mt-2 flex items-center' href='/'>
                 <span>Go to Home</span> <ChevronRight />
               </Link>
             </div>
             <Image
               src="/browse.svg" 
-              alt="AI Chat Icon"
+              alt="Browse Icon"
               className=""
               width={60}
               height={60}
@@ -50,18 +55,18 @@ export default function Home() {
           </div>
 
           <div
-            className='flex justify-between items-center p-10 rounded-lg'
+            className='flex justify-between items-center p-6 md:p-10 rounded-lg'
             style={{ backgroundColor: '#F8F9FB' }}
           >
             <div>
               <p className='text-lg font-medium'>I want to see my plans</p>
-              <Link className='flex text-gray-600 mt-2' href='/'>
-                <span> Bookings and itinerary </span> <ChevronRight />
+              <Link className='text-gray-600 mt-2 flex items-center' href='/'>
+                <span>Bookings and itinerary</span> <ChevronRight />
               </Link>
             </div>
             <Image
               src="/calendar.svg" 
-              alt="AI Chat Icon"
+              alt="Calendar Icon"
               className=""
               width={60}
               height={60}
