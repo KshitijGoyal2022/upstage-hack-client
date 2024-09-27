b52ab14472e5d635a48d6077d75dfa03fc77a816
Ishwak Sharda <ishwak.sharda@gmail.com>
2024-09-27 16:50:06 -0700

    feat: restaurants

--- src/components/AiPlayground.tsx
+++ src/components/AiPlayground.tsx
@@ -35,6 +35,20 @@ export const hotel_tags_set = new Set([
 	"mountain_hut",
 ]);
 
+function groupIntoPairs(arr: any[]) {
+	let result = [];
+
+	for (let i = 0; i < arr.length; i += 2) {
+		result.push([arr[i], arr[i + 1]]);
+	}
+
+	return result;
+}
+
+// Example usage:
+const array = [1, 2, 3, 4, 5, 6];
+const groupedArray = groupIntoPairs(array);
+console.log(groupedArray);
 export default function AiPlayground(props: {
 	itineraryId: string;
 	itinerary: any;
@@ -284,32 +298,75 @@ export default function AiPlayground(props: {
 								)}
 								{/**show me some restaurants in new york */}
 								{chat.restaurant_search?.local_results && (
-									<div>
-										{chat.restaurant_search.local_results.map((restaurant) => {
-											return (
-												<div key={restaurant.restaurant_id}>
-													<p>
-														{restaurant.title} - {restaurant.rating} (
-														{restaurant.reviews})
-													</p>
-													<p>
-														{restaurant.description || ""} - {restaurant.price}
-													</p>
-													<p>
-														{restaurant.address} - {restaurant.type}
-													</p>
-													<p>
-														{restaurant.distance} - {restaurant.hours}
-													</p>
-													<Image
-														src={restaurant.images[0]}
-														alt={restaurant.restaurant_id}
-														width={70}
-														height={70}
-													/>
-												</div>
-											);
-										})}
+									<div className="px-8 flex flex-row gap-8 overflow-x-auto pb-8">
+										{groupIntoPairs(chat.restaurant_search.local_results).map(
+											(pair) => {
+												return (
+													<div className="space-y-8">
+														{pair.map((restaurant) => {
+															return (
+																<div
+																	className="flex w-[500px] flex-row gap-4 border-box p-3 hover:bg-slate-50 cursor-pointer rounded-2xl"
+																	style={{ flex: "0 0 auto" }}
+																>
+																	<Image
+																		src={restaurant.images[0]}
+																		alt={restaurant.restaurant_id}
+																		width={144}
+																		height={144}
+																		objectFit="cover"
+																		className="rounded-xl w-[144px] shrink-0 h-[144px] bg-cover"
+																	/>
+																	<div>
+																		<h2 className="font-medium text-lg">
+																			{restaurant.title}
+																		</h2>
+																		<div className="flex items-center -ml-2 mt-0.5">
+																			<span className="text-xs ml-2 text-slate-600">
+																				{restaurant.rating}
+																			</span>
+																			{new Array(5).fill(0).map((_, index) => (
+																				<svg
+																					className={`w-3.5 h-3.5 ms-1 -mt-0.5 ${
+																						index < restaurant.rating
+																							? "text-yellow-300"
+																							: "text-gray-300 dark:text-gray-500" // text-yellow-500
+																					}`}
+																					aria-hidden="true"
+																					xmlns="http://www.w3.org/2000/svg"
+																					fill="currentColor"
+																					viewBox="0 0 22 20"
+																				>
+																					<path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+																				</svg>
+																			))}{" "}
+																			<span className="text-xs ml-2 text-slate-600">
+																				({restaurant.reviews}){" "}
+																				{restaurant.price
+																					? "· " + restaurant.price
+																					: ""}{" "}
+																				{restaurant.type
+																					? "· " + restaurant.type
+																					: ""}{" "}
+																			</span>
+																		</div>
+																		<h2 className="text-sm text-slate-500 mt-1">
+																			{restaurant.address}
+																		</h2>
+																		<h2 className="text-sm  text-slate-500 line-clamp-2">
+																			{restaurant.description}
+																		</h2>
+																		<h2 className="text-sm  text-slate-500 mt-1">
+																			{restaurant.distance} - {restaurant.hours}
+																		</h2>
+																	</div>
+																</div>
+															);
+														})}
+													</div>
+												);
+											}
+										)}
 									</div>
 								)}
 								{/**show me some museums in milan*/}
